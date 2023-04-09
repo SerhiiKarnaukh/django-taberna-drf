f875b80b82d47663d942924b8636fbac87f42da5
SerhiiKarnaukh <serhiikarnaukh1984@gmail.com>
2023-04-09 18:00:36 +0300

    Search added

--- taberna_drf/apps/product/views.py
+++ taberna_drf/apps/product/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.db.models import Count
 
 from .models import Product, Category
@@ -52,3 +54,16 @@ class ProductCategoryView(APIView):
             one__gt=0)
         serializer = AllCategoriesSerializer(categories, many=True)
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
